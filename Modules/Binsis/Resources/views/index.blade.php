643912f3cf3ab6d221f2f1ddf3cf6b0b83dbd506
Rezky <rezkymaulanaa06@gmail.com>
2021-10-26 15:31:26 +0700

    add Binsis module

---
+++ Modules/Binsis/Resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('binsis::layouts.master')
+
+@section('content')
+    <h1>Hello World</h1>
+
+    <p>
+        This view is loaded from module: {!! config('binsis.name') !!}
+    </p>
+@endsection
